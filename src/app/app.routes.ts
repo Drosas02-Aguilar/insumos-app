20ad4cb46acfee7c70ce823af0fa88d1efac57f7
Drosas02-Aguilar <damianra1802@outlook.com>
2026-04-24 12:50:13 -0600

    Configuracion Inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{path:'', redirectTo: 'home',pathMatch: 'full'},
+    {
+path: 'home',
+        loadComponent: ()=>
+            import('./pages/home/home').then(m=>m.Home)
+
+    },
+    {
+        path:'carga',
+        loadComponent: () =>
+            import('./pages/carga/carga').then(m=>m.Carga)
+    },
+    {
+        path:'consultas',
+        loadComponent: () =>
+            import('./pages/consultas/consultas').then(m=>m.Consultas)
+    },
+    {path:'**', redirectTo: 'home'}
+
+
+];
